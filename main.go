f45bfd55b9271ec534972332723f89a400116bb6
ozin <b1506231@planet.kanazawa-it.ac.jp>
2018-04-12 01:35:29 +0900

    update

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"github.com/nlopes/slack"
 	"os"
+	"net/http"
+	"fmt"
+	"github.com/nlopes/slack"
 	"github.com/JinOketani/slack_image_bot/cli"
 )
 
 func main() {
+	http.HandleFunc("/", server)
+	fmt.Println("listening...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func server(res http.ResponseWriter, req *http.Request) {
 	token := slack.New(os.Getenv("SLACK_API_TOKEN"))
 	os.Exit(cli.Run(token))
+	fmt.Fprintln(res, "success")
 }
